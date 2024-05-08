b514dc65ded836130dcc1d435575bd00edd37829
nquinbla <nquinbla@myuax.com>
2024-05-08 12:18:40 +0200

    main3 pt13 (no se que hago)

--- src/main/java/A_CuentaUsuario/Tweet.java
+++ src/main/java/A_CuentaUsuario/Tweet.java
@@ -18,10 +18,10 @@ public class Tweet {
 
     @Override
     public String toString() {
-        return "Tweet{" +
-                "message='" + message + '\'' +
-                ", time=" + time +
-                ", sender=" + sender +
+        return "Tweet:" +
+                "message:'" + message + '\'' +
+                ", time:" + time +
+                ", sender:" + sender +
                 '}';
     }
 }
